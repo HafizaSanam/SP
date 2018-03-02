82430ce1fc96fe35cc93874c9b2aa81c782645e3
HafizaSanam <36662982+HafizaSanam@users.noreply.github.com>
2018-03-02 15:54:31 +0000

    Update Cheat.sh

--- Cheat.sh
+++ Cheat.sh
@@ -19,6 +19,41 @@ then
 	else
 		echo "CHEATING: 1"
 	fi
+
+elif [ $# = 5 ]
+then
+	arr1=(`ls -l $2`)
+
+	arr2=(`ls -l $4`)	
+	
+	echo "OWNER: " ${arr1[2]}
+	echo "GROUP " ${arr1[3]}
+	echo "PERMISSIONS: " ${arr1[0]}
+	echo "FILENAME: " $2
+
+	echo "OWNER: " ${arr2[2]}
+	echo "GROUP: " ${arr2[3]}
+	echo "PERMISSIONS: " ${arr2[0]}
+	echo "FILENAME: " $4
+
+	echo "The difference between $2 and $4"
+
+	echo `diff -c $2 $4`
+	
+	 var=$?	
+
+	if [ $var = 1 ]
+
+	then
+		echo `diff -y $2 $4`
+		echo "CHEATING: 0"
+	else
+		echo "There is no difference"
+		echo "CHEATING: 1"
+	fi
+elif [ $# = 0 ]
+then
+	echo "No Arguments"
 else
-	echo "No arguments"
+	echo "Wrong arguments"
 fi
