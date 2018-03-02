93548eed10834ecae2c7e1c440d2ef8397449366
HafizaSanam <36662982+HafizaSanam@users.noreply.github.com>
2018-03-02 15:59:57 +0000

    Update Cheat.sh

--- Cheat.sh
+++ Cheat.sh
@@ -3,14 +3,10 @@
 if [ $# = 2 ]
 then
 
-	arrList=(`ls -l $1`)	
-	
+	arrList=(`ls -l $1`)
 	echo "OWNER: " ${arrList[2]}
-
 	echo "GROUP: " ${arrList[3]}
-	
 	echo "PERMISSIONS: " ${arrList[0]}
-
 	echo "FILENAME: " $1
 
 	if [ ${arrList[2]} = $2 ]
@@ -22,10 +18,8 @@ then
 
 elif [ $# = 5 ]
 then
-	arr1=(`ls -l $2`)
-
+	arr1=(`ls -l $2`
 	arr2=(`ls -l $4`)	
-	
 	echo "OWNER: " ${arr1[2]}
 	echo "GROUP " ${arr1[3]}
 	echo "PERMISSIONS: " ${arr1[0]}
@@ -39,11 +33,8 @@ then
 	echo "The difference between $2 and $4"
 
 	echo `diff -c $2 $4`
-	
 	 var=$?	
-
 	if [ $var = 1 ]
-
 	then
 		echo `diff -y $2 $4`
 		echo "CHEATING: 0"
